a5dacd309317e20187f189fb8559bce12db279fe
Peter Esiet <peteresiet92@gmail.com>
2020-09-28 00:11:57 +0100

    On branch master

--- .history/resources/js/script_20200725122635.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-
-    $('h1').click(function () {
-        $(this).css('backgroung-color', '#ff0000');
-    });
-});
--- resources/js/script.js
+++ resources/js/script.js
@@ -15,11 +15,11 @@ $(document).ready(function() {
     
     /* Scroll on buttons */
     $('.js--scroll-to-plans').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 2000); 
     });
     
     $('.js--scroll-to-start').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1500); 
     });
     
     
@@ -32,7 +32,7 @@ $(document).ready(function() {
         if (target.length) {
             $('html,body').animate({
             scrollTop: target.offset().top
-            }, 1000);
+            }, 2000);
             return false;
         }
         }
